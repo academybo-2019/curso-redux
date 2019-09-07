a52c36cb39c321354d9e2a23563b4f4284c3ca35
mario flores <react@gmail.com>
2019-09-07 10:39:08 -0400

    bookstore form

--- src/components/BookStore.js
+++ src/components/BookStore.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import {Label, Form, FormGroup, Input} from 'reactstrap';
 import {connect} from 'react-redux';
 
 class BookStore extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            id:null,
+            title:"",
+            description:""
+        }
+    }
+
+    handleSubmit(){
+        const newBook = {}
+        this.props.dispatch({type:'add',value:newBook});
+    }
+
+    handleChange(){}
+
     render() {
         console.log(this.props.books);
         return (
             <div>
-                <table>
+                <Form className="m-5" onSubmit={this.handleSubmit.bind(this)}>
+                    <FormGroup>
+                    <Label for="title">Title</Label>
+                    <Input type="text" name="title" value={this.state.title} onChange={(e)=>this.handleChange(e)} placeholder="with a placeholder" />
+                    </FormGroup>
+                    <FormGroup>
+                    <Label for="description">Description</Label>
+                    <Input type="text" name="description" value={this.state.description} onChange={(e)=>this.handleChange(e)} placeholder="password placeholder" />
+                    </FormGroup>
+                    <button >{this.state.update?'Guardar':'Registrar'}</button>
+                </Form>
+                <table className="m-5">
+                    <tbody>
                     {this.props.books && this.props.books.map((item) => {
                         return (
                             <tr key={item.id}>
@@ -18,6 +48,7 @@ class BookStore extends Component {
                             </tr>
                         );
                     })}
+                    </tbody>
                 </table>
             </div>
         );
@@ -29,4 +60,10 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookStore);
+function mapDispatchToProps(dispatch){
+    return {
+        dispatch
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookStore);
